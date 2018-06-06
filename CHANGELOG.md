ae9c093aa4bd78e1b175768e1230a1bd19b437f4
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-06-06 15:25:37 +0200

    Match Command#call and Command#valid signature

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.2.1
+
+- `assert_valid` and `refute_valid` take `*args`, to match `Command#call` (and thus `Command#valid?`)
--- lib/command/minitest/assertions/assert_valid.rb
+++ lib/command/minitest/assertions/assert_valid.rb
@@ -1,19 +1,19 @@
 module Minitest
   module Assertions
-    def assert_valid(actual)
-      assert valid?(actual),
+    def assert_valid(actual, *args)
+      assert valid?(actual, *args),
              "Expected #{actual.inspect} to be valid?"
     end
 
-    def refute_valid(actual)
-      refute valid?(actual),
+    def refute_valid(actual, *args)
+      refute valid?(actual, *args),
              "Expected #{actual.inspect} to not be valid?"
     end
 
     private
 
-    def valid?(actual)
-      actual.valid?
+    def valid?(actual, *args)
+      actual.valid?(*args)
     end
   end
 end
